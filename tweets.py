02e61fa3ec2099f6846efc8a4314a4f8fcd0ccc7
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-09-18 01:14:08 -0400

    Add files via upload

---
+++ tweets.py
@@ -0,0 +1,28 @@
+import wget
+
+filename = "fetched_tweets.json"
+file = open(filename)
+
+tweets = ['']
+count = 0
+
+substring = "media_url"
+for lines in file:
+	#print(lines)
+	if substring in lines:
+		print("Yes")
+		line_new = lines.split('"')
+		num = line_new.index('media_url')
+		media_url = line_new[num+2]
+		media_url = media_url.replace("\\", "")
+		#print(media_url)
+		img_name = "img" + str(count).zfill(4) + ".jpg"	
+		count = int(count) + 1
+		wget.download(media_url, out=img_name)
+	else:
+		print("No")
+
+
+
+
+
