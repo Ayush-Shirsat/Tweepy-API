dfcefd0c58be54be3a96481f680661466a4cc3c3
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-09-23 14:57:42 -0400

    Added Comments

--- get_images.py
+++ get_images.py
@@ -1,5 +1,8 @@
+#Step-2: Extract Media URLs from tweets(in json format) and download images.
+
 import wget
 
+#Access file that contains tweets
 filename = "fetched_tweets.json"
 file = open(filename)
 
@@ -9,15 +12,21 @@ count = 0
 substring = "media_url"
 for lines in file:
 	if substring in lines:
+		# Print Yes if tweet contains Media URL
 		print("Yes")
 		line_new = lines.split('"')
 		num = line_new.index('media_url')
 		media_url = line_new[num+2]
 		media_url = media_url.replace("\\", "")
+		
+		# Save downloaded images in format (imgXXXX.jpg)
 		img_name = "img" + str(count).zfill(4) + ".jpg"	
 		count = int(count) + 1
+		
+		# Download the images
 		wget.download(media_url, out=img_name)
 	else:
+		# Prints No if tweet does not contain Media URL
 		print("No")
 
 
