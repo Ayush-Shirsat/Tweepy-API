6b602a2c1b03ba16d957e26c68acc1875c90a968
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-09-23 17:10:29 -0400

    Added Comments

--- to_video.py
+++ to_video.py
@@ -1,8 +1,10 @@
 import os
 
 try:
+	# Frame rate is 1 fps and video is of format .mp4
 	command = "ffmpeg -r 1/1 -i img%04d.jpg -vcodec mpeg4 -y output.mp4"
 	os.system(command)
 except Exception as e:
+	# Throws error (if any)
 	print(e)
 	exit()
