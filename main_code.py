4e9c6fccfbf6528f4e012e6d70da9816214942d9
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-09-24 16:14:50 -0400

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -10,30 +10,46 @@ import twitter_credentials
 class StdOutListener(StreamListener):
 
 	def __init__(self):
+		# Initializing Tweet count to 0
 		self.counter = 0
-
-
+		
 	def on_data(self, data):
+		# Incrementing Tweet count
 		self.counter += 1
-		if self.counter > 200: # limit the stream to 200 tweets
+		
+		if self.counter > num:
+			# Exit code once Tweet limit is reached
 			sys.exit() 
 		else:
-			print(data) #Display tweets as they stream
-			with open('fetched_tweets.json','a+') as tf: #Save tweets in json format
+			# Prints Tweets(in json) as they stream
+			print(data)
+			
+			# Save all Tweets to a file
+			with open('fetched_tweets.json','a+') as tf:
 				tf.write(data)
 
 		return True
 
 	def on_error(self, status):
-		print(status) # Print error (if any)
+		# Prints error (if any) 
+		print(status)
 
 if __name__== "__main__":
 
 	listener = StdOutListener()
 	auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 	auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
+	
+	# Takes user input for maximum Tweets to Analyze
+	num = int(input("Enter maximum number of tweets to analyze: ")) 
+	
+	# Takes user input to search for a keyword while streaming Tweets
+	word = input("Enter a keyword to search: ")
+	
 	stream = Stream(auth, listener)
-  
-	stream.filter(track=['football']) # Set category of tweets by filtering track
+	
+	# Filters search track based on user keyword
+	stream.filter(track=[word])
+
 end
+
